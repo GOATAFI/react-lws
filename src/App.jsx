db8ada148d42a7009807d073195625a4616604c7
GOATAFI <projectexecutiveofpm@gmail.com>
2024-01-13 17:00:44 +0600

    transforming an array by map

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,47 @@
 import { useState } from "react";
 
-let initialArtists = [
-  { id: 0, name: 'Marta Colvin Andrade' },
-  { id: 1, name: 'Lamidi Olonade Fakeye'},
-  { id: 2, name: 'Louise Nevelson'},
-];
+let initialShapes =[
+  {id:0,type:'circle',x:50,y:100},
+  {id:1,type:'square',x:150,y:100},
+  {id:2,type:'circle',x:250,y:100},
+]
 
-
-const List = () => {
-  // const [name, setName] = useState("");
-  const [artists, setArtists] = useState(initialArtists);
+const ShapeEditor = () => {
+  const [shapes,setShapes] = useState(initialShapes);
+  function handleClick() {
+    const nextShapes = shapes.map(s =>{
+      if(s.type === 'square'){
+        return s;
+      }
+      else{
+        return {
+          ...s,
+          y:s.y +50
+        }
+      }
+    })
+    setShapes(nextShapes)
+  }
   return (
     <div>
-      <h1>Sculptures: </h1>
-      <ul>
-        {artists.map(artist => (
-          <li key={artist.id}>{artist.name}
-          <button onClick={()=>{
-            setArtists(
-              artists.filter(a => a.id !== artist.id)
-            )
-          }}>
-            Delete
-          </button>
-          </li>
-        ))}
-      </ul>
-      {/* <ul>
-        {artists.map((artist) => (
-          <li key={artist.id}>{artist.name}</li>
-        ))}
-      </ul> */}
+      <button onClick={handleClick}>Move circles down</button>
+      {shapes.map(shape => (
+        <div
+          key={shape.id}
+          style={{
+          background: 'purple',
+          position: 'absolute',
+          left: shape.x,
+          top: shape.y,
+          borderRadius:
+            shape.type === 'circle'
+              ? '50%' : '',
+          width: 20,
+          height: 20,
+        }} />
+      ))}
     </div>
   );
 };
 
-export default List;
+export default ShapeEditor;
